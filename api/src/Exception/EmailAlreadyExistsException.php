3eaf14d93721969e3e00cd110de421677f539e82
JB <ecireajb@gmail.com>
2026-05-05 11:43:19 +0200

    Update EmailAlreadyExistsException

--- api/src/Exception/EmailAlreadyExistsException.php
+++ api/src/Exception/EmailAlreadyExistsException.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Exception;
 
-class EmailAlreadyExistsException extends \RuntimeException
+use RuntimeException;
+
+class EmailAlreadyExistsException extends RuntimeException
 {
     public function __construct(string $email)
     {
